42a408de9947c7752d89ce4f8ca48245aceeac19
Victor Robu <victor.robu.ext@extendaretail.com>
2022-07-01 15:46:59 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,6 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Nello Calculator!!!");
+        System.out.println("Hello Calculator!!!");
     }
 }
