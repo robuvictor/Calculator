90bb64a485039e33937143b7aae9c3fb3276b8b1
robuvictor <victor.robu.ext@extendaretail.com>
2022-07-01 15:42:55 +0300

    First commit in the 'Calculator' project

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Nello Calculator!!!");
+    }
+}
